b3cbeca47fd0d624a15b8e43a1af5d6037a07a1b
Eric Tummers <eric.tummers@valid.nl>
2015-02-02 09:15:43 +0100

    Initial code checking

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace FancontrolLogger
+{
+    /// <summary>
+    /// Download and install Notebook FanControl (http://www.computerbase.de/forum/showthread.php?t=1070494)
+    /// and make sure the backend service is running
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a model to hold the CPU information
+            var model = new NbfcServiceClient.ViewModels.MainWindowViewModel();
+            // print out the CpuTemperature when changed
+            model.PropertyChanged += (s, e) => { 
+                if (e.PropertyName.Equals("CpuTemperature")) 
+                    Console.WriteLine("Temp is {0}C", model.CpuTemperature); 
+            };
+
+            // update intervals
+            var updateIntervalInSeconds = 10;
+            var updateIntervalInTimeSpan = TimeSpan.FromSeconds(updateIntervalInSeconds);
+            var client = new NbfcServiceClient.FanControlClient(model, updateIntervalInSeconds);
+            client.StartFanControl();
+
+            // stop recursive calling and end the program
+            bool stop = false;
+
+            // read the import on another thread
+            // loop below does not allow user input
+            Task.Factory.StartNew(() => { 
+                Console.ReadLine(); 
+                Console.WriteLine("Stopping ..."); 
+                stop = true; 
+            });
+
+            // until ReadLine in Task above 
+            while (!stop) {
+                // update view model to trigger changes
+                Task.Factory.StartNew(() => {
+                    client.UpdateViewModel();
+                    Thread.Sleep(updateIntervalInTimeSpan);
+                }).Wait();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FancontrolLogger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FancontrolLogger")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2b58445-0061-45b2-81b5-d4bd963d05c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
